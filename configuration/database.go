893e551f6cd89db7d98c1337dc29546c24f42fa9
Bruno de Oliveira Macedo <bruno.deoliveiramacedo-ext@ab-inbev.com>
2020-11-09 01:25:25 -0300

    Code improvement

--- configuration/database.go
+++ configuration/database.go
@@ -18,7 +18,7 @@ var collection *mongo.Collection
 var ctx = context.TODO()
 
 func ConnectToDB() *mongo.Client {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://db:27017")
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		log.Fatal(err)
